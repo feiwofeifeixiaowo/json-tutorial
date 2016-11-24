2ef7fe05611a01534b9052f5a5d6c18eaea8727a
F <feiwofeifeixiaowo@gmail.com>
2016-11-24 13:06:58 +0800

    add comments, delete files, finish tutorial02 exe1

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -15,6 +15,7 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
+// DEPRECATED use lept_parse_literal instead
 static int lept_parse_true(lept_context* c, lept_value* v) {
     EXPECT(c, 't');
     if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
@@ -24,6 +25,7 @@ static int lept_parse_true(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+// DEPRECATED use lept_parse_literal instead
 static int lept_parse_false(lept_context* c, lept_value* v) {
     EXPECT(c, 'f');
     if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
@@ -33,12 +35,35 @@ static int lept_parse_false(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+// DEPRECATED use lept_parse_literal instead
 static int lept_parse_null(lept_context* c, lept_value* v) {
     EXPECT(c, 'n');
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
     c->json += 3;
     v->type = LEPT_NULL;
+    return LEPT_PARSE_OK;
+}
+
+// merge parse null, true, false into one parse literal 
+// lept_context* c  : data to parse
+// lept_value*v     : storage parse result
+// const char* type : parse type : true/false/null
+// int size         : size of parse type  
+static int lept_parse_literal(lept_context* c, lept_value* v, const char* type, int size) {
+    EXPECT(c, *type);
+    for (int i = 0; i < size-1; i++) {
+        if (c->json[i] != type[i+1]) {
+            return LEPT_PARSE_INVALID_VALUE;   
+        }
+    }
+    c->json += (size-1);
+    if (*type == 'n')
+        v->type = LEPT_NULL;
+    else if (*type == 'f')
+        v->type = LEPT_FALSE;
+    else if (*type == 't')
+        v->type = LEPT_TRUE;
     return LEPT_PARSE_OK;
 }
 
@@ -54,10 +79,13 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
+    char type_true[] = "true";
+    char type_false[] = "false";
+    char type_null[] = "null";
     switch (*c->json) {
-        case 't':  return lept_parse_true(c, v);
-        case 'f':  return lept_parse_false(c, v);
-        case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_literal(c, v, type_true,  4);
+        case 'f':  return lept_parse_literal(c, v, type_false, 5);
+        case 'n':  return lept_parse_literal(c, v, type_null,  4);
         default:   return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
     }
--- tutorial02/leptjson.h
+++ tutorial02/leptjson.h
@@ -4,6 +4,7 @@
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
 typedef struct {
+    // storage json number type
 	double n;
     lept_type type;
 }lept_value;
@@ -13,13 +14,15 @@ enum {
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
     LEPT_PARSE_ROOT_NOT_SINGULAR,
-    LEPT_PARSE_NUMBER_TOO_BIG
+    LEPT_PARSE_NUMBER_TOO_BIG  // number out of range which leptjson can parse
 };
 
 int lept_parse(lept_value* v, const char* json);
 
 lept_type lept_get_type(const lept_value* v);
 
+// return number value 
+// lept_value* v : struct to storage return value
 double lept_get_number(const lept_value* v);
 
 #endif /* LEPTJSON_H__ */
